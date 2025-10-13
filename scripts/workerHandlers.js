89d48e4b13ccfa7bb20478c63c83e913b59ea564
ConvenienteCoder <convenientetecnologia@gmail.com>
2025-10-13 13:29:02 -0300

    [atualização]

--- scripts/workerHandlers.js
+++ scripts/workerHandlers.js
@@ -125,7 +125,7 @@ return { ok: false, error: 'Core não expôs robePlay' };
 return await core.robePlay({ nome });
 },
 
-async 'robes-release-all' {
+a'robes-release-all': async function() {
 if (typeof core.robesReleaseAll !== 'function') {
 return { ok: false, error: 'Core não expôs robesReleaseAll' };
 }
