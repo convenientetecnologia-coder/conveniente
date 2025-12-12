facba9cc8f34b7348ea4c6e2a1bffa7494669391
ConvenienteCoder <convenientetecnologia@gmail.com>
2025-12-12 13:37:39 -0300

    [atualização]

--- scripts/exportProjetoAtendimento.js
+++ scripts/exportProjetoAtendimento.js
@@ -18,17 +18,13 @@ const OUT_FILE = path.join(ROOT, "projeto atendimento.txt");
 const ATENDIMENTO_FILES = [
   "index.js",
   "scripts/api_perfis.js",
-  "scripts/api_issues.js",
   "scripts/browser.js",
   "scripts/chatLock.js",
-  "scripts/chatStore.js",
   "scripts/inteligenciaArtificial.js",
-  "scripts/issues.js",
   "scripts/logger.js",
   "scripts/manifestStore.js",
   "scripts/promptFretes.js",
   "scripts/stepLog.js",
-  "scripts/utils.js",
   "scripts/virtus.js",
   "scripts/worker.js",
 ];
